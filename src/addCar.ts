5c6f4b465e1663b0d71ea293711039f77ea5b629
Igor Danilov <danilovigor1999@gmail.com>
2023-01-22 20:05:39 +0300

    fix:butt start

---
+++ src/addCar.ts
@@ -0,0 +1,13 @@
+export default function addingCars() {
+    var i = 0
+    document.querySelector('.create').addEventListener('click', () =>{
+        i++
+        let road = `<div class="blockCar"><div class="settInfo"><button class="select${i}">Select</button><button class="remove">Remove</button><div class="namCar${i}"></div></div><div class="road"><div class="startStop"><button class="start${i} ">A</button><button class="stop${i}" disabled/>B</button></div><div class="way"> <img class="carImg carColor${i}" src="assets/car.png" alt="car"><img class="flag" src="/assets/flag.png" alt="flag"></div></div></div>`
+    let cars = document.createElement(`blockCar${i}`)
+    cars.innerHTML = road 
+    document.querySelector(".cars").appendChild(cars) 
+    document.querySelector(`.namCar${i}`).textContent =  document.querySelector<HTMLInputElement>('.inpCreat').value
+    document.querySelector<HTMLInputElement>(`.carColor${i}`).style.backgroundColor =  document.querySelector<HTMLInputElement>(".createColor").value
+})
+}
+
--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,14 @@
 import './style.scss'
+import {win,garage} from './views'
+import addingCars from './addCar'
+import startStop from './startStop'
+
+win()
+garage()
+addingCars()
+startStop()
 
 
 
-    var i = 1
-    let road = `<div class="blockCar"><div class="settInfo"><button class="select">Select</button><button class="remove">Remove</button><div class="namCar">Volga</div></div><div class="road"><div class="startStop"><button class="start">A</button><button class="stop" disabled/>B</button></div><div class="way"> <img class="carImg" src="assets/car.png" alt="car"><img class="flag" src="/assets/flag.png" alt="flag"></div></div></div>`
-    
-    
-    document.querySelector(".cars").innerHTML = road
-    
-    document.querySelector('.start').addEventListener('click', function() { 
-        document.querySelector<SVGStyleElement>('.stop').disabled =  false
-        document.querySelector<SVGStyleElement>('.start').disabled =  true
-    })
-
-document.querySelector('.buttWinners').addEventListener('click', ()=>{
-    document.querySelector<HTMLElement>('.garage').style.display  = 'none'
-    document.querySelector<HTMLElement>('.garage1').style.display  = 'none'
-    document.querySelector<HTMLElement>('.winners').style.display  = ''
-})
-document.querySelector('.buttGarage').addEventListener('click', ()=>{
-    document.querySelector<HTMLElement>('.winners').style.display  = 'none'
-    document.querySelector<HTMLElement>('.garage').style.display  = ''
-    document.querySelector<HTMLElement>('.garage1').style.display  = ''
-})
-
-document.querySelector('.create').addEventListener('click',()=>{
-    i++
-    let road2 = `<div class="blockCar"><div class="settInfo"><button class="select">Select</button><button class="remove">Remove</button><div class="namCar${i}"></div></div><div class="road"><div class="startStop"><button class="start${i}">A</button><button class="stop${i}" disabled/>B</button></div><div class="way"> <img class="carImg carColor${i}" src="assets/car.png" alt="car"><img class="flag" src="/assets/flag.png" alt="flag"></div></div></div>`
-    let cars = document.createElement(`blockCar${i}`)
-    cars.innerHTML = road2 
-    document.querySelector(".cars").appendChild(cars) 
-    document.querySelector(`.namCar${i}`).textContent =  document.querySelector<HTMLInputElement>('.inpCreat').value
-    document.querySelector<HTMLInputElement>(`.carColor${i}`).style.backgroundColor =  document.querySelector<HTMLInputElement>(".createColor").value
-})
 
 
