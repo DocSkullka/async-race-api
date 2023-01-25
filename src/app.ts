3ed19a31dfe6e84021db94fc05ea9f7047de1c1f
DocSkullka <docskullka@gmail.com>
2023-01-26 00:10:01 +0300

    adding:paggin

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import './style.scss'
 import {win,garage} from './views'
 import addingCars from './addCar'
 import {startStop, race, reset} from './startStop'
+import { paggination, nextBackButton } from './paggination'
 
 win()
 garage()
-addingCars()
 startStop()
 race()
 reset()
+paggination()
+nextBackButton()
+addingCars()
 
-alert('Здравствуйте, не могли бы вы проверить мою работу в конце кросс чека. Заранее спасибо <3')
+// alert('Здравствуйте, не могли бы вы проверить мою работу в конце кросс чека. Заранее спасибо <3')
